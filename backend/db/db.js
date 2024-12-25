4ed5e490e65d016c5a0d2d3659b3469862ead507
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2024-12-25 11:40:32 +0530

    db port added

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: process.env.DB_PORT,
 });
 
 sequelize.authenticate()
