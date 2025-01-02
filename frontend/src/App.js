b70227bcbebbf954be0a5c56cce5d2dfddd17430
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-02 19:58:35 +0530

    designed error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,6 +59,7 @@ function App() {
             <Route index element={<Admin />} />
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="newProject" element={<NewProjectPage />} />
+            <Route path="*" element={<Error />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/error/Error.jsx
+++ frontend/src/pages/error/Error.jsx
@@ -1,13 +1,19 @@
-import './style.scss'
+import './style.scss';
 
-//Importing Components
+// Importing Components
 
 const Error = () => {
   return (
-    <div>
-      Error
+    <div className="error-container">
+      <div className="error-content">
+        <h1 className="error-heading">404</h1>
+        <p className="error-message">Oops! The page you're looking for doesn't exist.</p>
+        <button className="go-back-btn" onClick={() => window.history.back()}>
+          Go Back
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
