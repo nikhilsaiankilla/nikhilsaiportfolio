5fba17b35ea55c9777cdc21aa7ef787aea52a3fd
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-02 20:21:13 +0530

    deleted unwanted images

--- frontend/src/assests/index.js
+++
@@ -1,9 +0,0 @@
-import project1 from "./mirinda.png";
-import gpt from "./gpt-3.png";
-import spotify from "./spotify.png";
-import portfolio from "./portfolio.png";
-import ecom from "./ecom.png";
-import blog from './blog.webp'
-import moviesApp from './movieApp.webp'
-
-export { project1, gpt, spotify, portfolio, ecom, blog, moviesApp };
